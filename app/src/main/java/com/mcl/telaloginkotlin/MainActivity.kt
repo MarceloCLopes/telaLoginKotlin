7323c14c6a023302d9c3a8b8c044ec6d94ca0074
MarceloCLopes <marc01mcl@gmail.com>
2021-03-15 18:47:30 -0300

    implementation navigation fragment login

--- app/src/main/java/com/mcl/telaloginkotlin/MainActivity.kt
+++ app/src/main/java/com/mcl/telaloginkotlin/MainActivity.kt
@@ -2,10 +2,32 @@ package com.mcl.telaloginkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.mcl.telaloginkotlin.ui.LoginFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationHost {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null){
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.container, LoginFragment())
+                .commit()
+        }
+    }
+
+    override fun navigateTo(fragment: Fragment, addToBackstack: Boolean) {
+        val transaction : FragmentTransaction = supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container, fragment)
+
+        if (addToBackstack){
+            transaction.addToBackStack(null)
+        }
+
+        transaction.commit()
     }
 }
